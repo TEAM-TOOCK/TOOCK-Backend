676a1ff3371b41ca7109dfd674c5221560ba6fd7
김민석 <skyfox001015@naver.com>
2025-07-18 11:48:52 +0900

    Initial commit

---
+++ src/main/java/toock/backend/ToockBackendApplication.java
@@ -0,0 +1,13 @@
+package toock.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toock/backend/ToockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package toock.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
