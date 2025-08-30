af70d47329d7245737794ab0b52d3667a33c5405
김민석 <skyfox001015@naver.com>
2025-08-31 01:41:04 +0900

    fix : 인자주입에서, 상대 경로로 변경

--- src/main/java/toock/backend/batch/DataLoaderRunner.java
+++ src/main/java/toock/backend/batch/DataLoaderRunner.java
@@ -12,21 +12,16 @@ public class DataLoaderRunner implements CommandLineRunner {
 
     private final CompanyReviewBatchService batchService;
 
+    private static final String CSV_FOLDER_PATH = "src/main/resources/csv/";
+
     @Override
     public void run(String... args) throws Exception {
-        if (args.length == 0) {
-            System.err.println("❌ CSV 파일이 있는 폴더 경로를 입력해주세요.");
-            System.err.println("✅ 실행 예시: ... --args=\"C:/your/csv/folder\"");
-            return;
-        }
-
-        String csvFolderPath = args[0];
         System.out.println("==================================================");
         System.out.println("🚀 CSV 데이터 로딩 작업을 시작합니다.");
-        System.out.println("📂 대상 폴더: " + csvFolderPath);
+        System.out.println("📂 대상 폴더: " + CSV_FOLDER_PATH);
         System.out.println("==================================================");
 
-        batchService.loadAllCsvsInDirectory(csvFolderPath);
+        batchService.loadAllCsvsInDirectory(CSV_FOLDER_PATH);
 
         System.out.println("==================================================");
         System.out.println("🎉 모든 CSV 파일 처리가 완료되었습니다.");
